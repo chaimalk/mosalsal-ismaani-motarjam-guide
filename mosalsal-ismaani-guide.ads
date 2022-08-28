c78fbef99978845aeca64376392bc03ba347d3be
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-08-28 04:00:20 +0200

    Create mosalsal-ismaani-guide.ads

---
+++ mosalsal-ismaani-guide.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "unity",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/4244405022",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/7992078347",
+
+			"BannerFacebook": "1281693222654734_1281693672654689",
+			"InterstitialFacebook": "1281693222654734_1281693969321326",
+			
+			"unityGameID": "4904667",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "207976187",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
